2dcea788914aad19740a1637018a8e60d0e94d10
root <cocalir@gmail.com>
2019-04-28 01:07:25 +0300

    Updates

---
+++ precache-manifest.1bf18d259f6123257e4029e7dce04195.js
@@ -0,0 +1,98 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "bf605c0415da0da0ae532d3f2fd72758",
+    "url": "/immflyfrontend/index.html"
+  },
+  {
+    "revision": "3b4a331f542e3e04285b",
+    "url": "/immflyfrontend/static/css/2.f0feb1ce.chunk.css"
+  },
+  {
+    "revision": "a10368ccfb138ab1d4a0",
+    "url": "/immflyfrontend/static/css/main.5706caca.chunk.css"
+  },
+  {
+    "revision": "3b4a331f542e3e04285b",
+    "url": "/immflyfrontend/static/js/2.89b1d2ee.chunk.js"
+  },
+  {
+    "revision": "a10368ccfb138ab1d4a0",
+    "url": "/immflyfrontend/static/js/main.a867a372.chunk.js"
+  },
+  {
+    "revision": "56c74519396cf4a1c06c",
+    "url": "/immflyfrontend/static/js/runtime~main.8fafd13e.js"
+  },
+  {
+    "revision": "13db00b7a34fee4d819ab7f9838cc428",
+    "url": "/immflyfrontend/static/media/brand-icons.13db00b7.eot"
+  },
+  {
+    "revision": "a046592bac8f2fd96e994733faf3858c",
+    "url": "/immflyfrontend/static/media/brand-icons.a046592b.woff"
+  },
+  {
+    "revision": "a1a749e89f578a49306ec2b055c073da",
+    "url": "/immflyfrontend/static/media/brand-icons.a1a749e8.svg"
+  },
+  {
+    "revision": "c5ebe0b32dc1b5cc449a76c4204d13bb",
+    "url": "/immflyfrontend/static/media/brand-icons.c5ebe0b3.ttf"
+  },
+  {
+    "revision": "e8c322de9658cbeb8a774b6624167c2c",
+    "url": "/immflyfrontend/static/media/brand-icons.e8c322de.woff2"
+  },
+  {
+    "revision": "9c74e172f87984c48ddf5c8108cabe67",
+    "url": "/immflyfrontend/static/media/flags.9c74e172.png"
+  },
+  {
+    "revision": "0ab54153eeeca0ce03978cc463b257f7",
+    "url": "/immflyfrontend/static/media/icons.0ab54153.woff2"
+  },
+  {
+    "revision": "8e3c7f5520f5ae906c6cf6d7f3ddcd19",
+    "url": "/immflyfrontend/static/media/icons.8e3c7f55.eot"
+  },
+  {
+    "revision": "962a1bf31c081691065fe333d9fa8105",
+    "url": "/immflyfrontend/static/media/icons.962a1bf3.svg"
+  },
+  {
+    "revision": "b87b9ba532ace76ae9f6edfe9f72ded2",
+    "url": "/immflyfrontend/static/media/icons.b87b9ba5.ttf"
+  },
+  {
+    "revision": "faff92145777a3cbaf8e7367b4807987",
+    "url": "/immflyfrontend/static/media/icons.faff9214.woff"
+  },
+  {
+    "revision": "dc7f4fa0bffd645e509237d5d091a60b",
+    "url": "/immflyfrontend/static/media/immfly-logo-white.dc7f4fa0.png"
+  },
+  {
+    "revision": "701ae6abd4719e9c2ada3535a497b341",
+    "url": "/immflyfrontend/static/media/outline-icons.701ae6ab.eot"
+  },
+  {
+    "revision": "82f60bd0b94a1ed68b1e6e309ce2e8c3",
+    "url": "/immflyfrontend/static/media/outline-icons.82f60bd0.svg"
+  },
+  {
+    "revision": "ad97afd3337e8cda302d10ff5a4026b8",
+    "url": "/immflyfrontend/static/media/outline-icons.ad97afd3.ttf"
+  },
+  {
+    "revision": "cd6c777f1945164224dee082abaea03a",
+    "url": "/immflyfrontend/static/media/outline-icons.cd6c777f.woff2"
+  },
+  {
+    "revision": "ef60a4f6c25ef7f39f2d25a748dbecfe",
+    "url": "/immflyfrontend/static/media/outline-icons.ef60a4f6.woff"
+  },
+  {
+    "revision": "b57a5cc716e4c82861ccfd5484f267d7",
+    "url": "/immflyfrontend/static/media/thy.b57a5cc7.png"
+  }
+]);
